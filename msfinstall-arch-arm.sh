6539f605f1f9219f147183c4bff9ecd649dc1b32
Pernat1y <1ns@pm.me>
2020-12-03 21:42:50 +0200

    Update msfinstall-arch-arm.sh

--- msfinstall-arch-arm.sh
+++ msfinstall-arch-arm.sh
@@ -20,14 +20,18 @@ pacman -Syyu \
 
 # Clone repo
 cd /opt
+rm -rf "/opt/metasploit-framework" 2>/dev/null
 git clone https://github.com/rapid7/metasploit-framework.git
-cd "metasploit-framework"
+cd "/opt/metasploit-framework"
 
 # Install gems
 CFLAGS+=" -I/usr/include/libxml2"
 bundle config set no-cache 'true'
 bundle config set deployment 'true'
 bundle install -j"$(nproc)"
+
+# Cleanup
+rm /opt/metasploit-framework/msfupdate 2>/dev/null
 
 # Add this to profile to make changes permanent
 PATH=$PATH:/opt/metasploit-framework
